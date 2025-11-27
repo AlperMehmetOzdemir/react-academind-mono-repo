c70c69bc4d96f6feea3380ceea528d16919353c5
Alper Mehmet Ozdemir <amehmet97@hotmail.com>
2025-11-27 17:17:23 +0300

    Initial commit up to Section 3 lesson 46 'More prop syntaxes'.

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,58 @@
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data';
+
+
+const reactDescriptions = ['Fundamental', 'Core', 'Crucial']
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  )
+}
+
+function CoreConept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((concept, index) => (
+              <CoreConept
+                key={index}
+                {...CORE_CONCEPTS[index]}
+              />
+            ))}
+          </ul>
+        </section>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
